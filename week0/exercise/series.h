c5efed60b405b6d1c1e30fcdb2675e0c4a251eff
Agent-Christmas <57256611+Agent-Christmas@users.noreply.github.com>
2019-11-07 19:35:23 +0800

    Update series.h

--- week0/exercise/series.h
+++ week0/exercise/series.h
@@ -1,1 +1,11 @@
+#ifndef WEEK1_SERIES_H
+#define WEEK1_SERIES_H
+#include <time.h>
+#include <math.h>
+#include <iostream>
+double f_1(int n, double a[], double x);
+double f_2(int n, double a[], double x);
+void duration1();
+void duration2();
+#endif
 
