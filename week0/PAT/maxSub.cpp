6ac22974d50be174f80d0930279ef9f85833f335
Agent-Christmas <57256611+Agent-Christmas@users.noreply.github.com>
2019-11-07 19:23:34 +0800

    Update maxSub.cpp

--- week0/PAT/maxSub.cpp
+++ week0/PAT/maxSub.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream>
+const int NUM = 100000;
+int maxSubSum(const int a[], int n);
+int main(){
+    int a[NUM];
+    int n;
+    std::cin >> n;
+    for( int i = 0; i < n; i++){
+        scanf("%d", a + i); // std::cin >> a[i];
+    }
+    int max = maxSubSum(a, n);
+    std::cout << max;
+    return 0;
+}
+int maxSubSum(const int a[], int n){
+    int currentSum = 0;
+    int maxSum = 0;
+    for (int i = 0; i < n; i++){
+        currentSum += a[i];
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+        else if (currentSum < 0)
+            currentSum = 0;
+    }
+    return maxSum;
+}
